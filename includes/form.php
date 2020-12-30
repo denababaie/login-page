51419690140b46f83c97cf0ffa590bf98d4f4009
denababaie <72995019+denababaie@users.noreply.github.com>
2020-12-30 12:46:39 +0100

    html css

---
+++ includes/form.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Form Page</title>
+</head>
+
+<body>
+
+<header> 
+<p class= headertitle>frontend & backend</p>
+</header>
+
+
+	<div class="container" id="container">
+		<div class="form-container log-in-container">
+			<form action="#">
+				<h1>Restricted access</h1>
+                <h7>Please enter your credentials</h7>
+				<div class="social-container">
+				</div>
+				<input type="email" placeholder="Email" />
+				<input type="password" placeholder="Password" />
+				<button>Log In</button>
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-right">
+					<h1>Login </h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed non risus. 
+						Suspendisse lectus tortor.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      Copyright <strong> My super protected website</strong> 
+    </p>
+  </div>
+</footer>
+
+
+</body>
+
+
+</html>
---
+++ includes/profile.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="styleprofile.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Profile </title>
+</head>
+
+<body>
+
+<header> 
+<p class= headertitle>frontend & backend</p>
+</header>
+
+	<div class="container" id="container">
+		<div class="form-container log-in-container">
+			<form action="#">
+				<img class= imageprofile>
+		<img src="img.jpeg" width=389 height=589>
+	</img>
+                
+            
+
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-right">
+					<h1>Hi, Slavoj</h1>
+					<h2>@zizek</h1>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
+						Suspendisse lectus tortor, dignissim sit amet,adipiscing nec, ultricies sed, dolor.
+						</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      Copyright <strong> My super protected website</strong> 
+    </p>
+  </div>
+</footer>
+
+
+</body>
+
+
+</html>
